8a9a87f8564a526fa1213f09dc252bccd77f6541
Terrance Rose Jr <trose618@gmail.com>
2018-10-20 19:10:38 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,9 +8,9 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total(card_total)
+def display_card_total(total)
   # code #display_card_total here
-  puts "Your cards add up to #{card_total}."
+  puts "Your cards add up to #{total}."
 end
 
 def prompt_user
