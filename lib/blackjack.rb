2f66b4f31d8312f67b869c026c78de299a1e13c4
Terrance Rose Jr <trose618@gmail.com>
2018-10-20 18:13:13 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,10 @@ def get_user_input
   input = gets
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  "Sorry, you hit #{card_total}. Thanks for playing!"
+  
 end
 
 def initial_round
